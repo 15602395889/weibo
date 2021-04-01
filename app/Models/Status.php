0e462b8bffb8ad4473add0df66124715f78e2172
vagrant <vagrant@homestead>
2021-04-01 07:49:25 +0000

    用户微博列表

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Status extends Authenticatable
+class Status extends Model
 {
     use HasFactory;
 
@@ -14,8 +14,4 @@ class Status extends Authenticatable
         return $this->belongsTo(User::class);
     }
 
-    public function statuses()
-    {
-        return $this->hasMany(Status::class);
-    }
 }
